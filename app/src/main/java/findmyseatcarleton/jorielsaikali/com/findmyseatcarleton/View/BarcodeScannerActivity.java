198e20d979984eecd77abcc64cd88b62ab174fef
jorisaikali <jori.saikali@hotmail.com>
2019-03-03 11:50:35 -0500

    Added BarcodeScanner classes (BarcodeScannerActivity, BarcodeScannerViewModel, BarcodeScannerModel)

---
+++ app/src/main/java/findmyseatcarleton/jorielsaikali/com/findmyseatcarleton/View/BarcodeScannerActivity.java
@@ -0,0 +1,15 @@
+package findmyseatcarleton.jorielsaikali.com.findmyseatcarleton.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import findmyseatcarleton.jorielsaikali.com.findmyseatcarleton.R;
+
+public class BarcodeScannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode_scanner);
+    }
+}
---
+++ app/src/main/java/findmyseatcarleton/jorielsaikali/com/findmyseatcarleton/ViewModel/BarcodeScannerViewModel.java
@@ -0,0 +1,7 @@
+package findmyseatcarleton.jorielsaikali.com.findmyseatcarleton.ViewModel;
+
+import android.arch.lifecycle.ViewModel;
+
+public class BarcodeScannerViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
