5c074cc201ba4180d6763bb158503cc33fd9d728
jorisaikali <jori.saikali@hotmail.com>
2019-03-08 22:02:16 -0500

    Skeleton for Settings and its children (i.e. ProfileSettings, Credits)

---
+++ app/src/main/java/findmyseatcarleton/jorielsaikali/com/findmyseatcarleton/View/ProfileSettingsFragment.java
@@ -0,0 +1,36 @@
+package findmyseatcarleton.jorielsaikali.com.findmyseatcarleton.View;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import findmyseatcarleton.jorielsaikali.com.findmyseatcarleton.R;
+import findmyseatcarleton.jorielsaikali.com.findmyseatcarleton.ViewModel.ProfileSettingsViewModel;
+
+public class ProfileSettingsFragment extends Fragment {
+
+    private ProfileSettingsViewModel mViewModel;
+
+    public static ProfileSettingsFragment newInstance() {
+        return new ProfileSettingsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.profile_settings_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ProfileSettingsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/findmyseatcarleton/jorielsaikali/com/findmyseatcarleton/ViewModel/ProfileSettingsViewModel.java
@@ -0,0 +1,7 @@
+package findmyseatcarleton.jorielsaikali.com.findmyseatcarleton.ViewModel;
+
+import android.arch.lifecycle.ViewModel;
+
+public class ProfileSettingsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
